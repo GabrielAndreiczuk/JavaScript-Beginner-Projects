a0f40246dbddebbc104f06925bfcbcface08328d
GabrielAndreiczuk <gabriel.andreiczuk@gmail.com>
2024-10-08 19:29:17 -0300

    Ajustes mensagem resultado

--- Calculadora IMC/src/javascript/imc.js
+++ Calculadora IMC/src/javascript/imc.js
@@ -10,7 +10,35 @@ function imc()
     if(nome !== '' && altura !== '' && peso !== '')
     {
         const resultado_imc = (peso / (altura * altura)).toFixed(2);
-        resultado.textContent = nome + " seu imc é de " + resultado_imc;
+
+        let classificacao;
+
+        if(resultado_imc < 18.5)
+        {
+            classificacao = "abaixo do peso!";
+        }
+        else if(resultado_imc < 25)
+        {
+            classificacao = "com o peso normal!";
+        }
+        else if (resultado_imc < 30) 
+        {
+            classificacao = "com sobrepeso!";
+        } 
+        else if (resultado_imc < 35) 
+        {
+            classificacao = "com obesidade grau I!";
+        } 
+        else if (resultado_imc < 40) 
+        {
+            classificacao = "com obesidade grau II!";
+        } 
+        else 
+        {
+            classificacao = "com obesidade grau III (mórbida)!";
+        }
+
+        resultado.textContent = `${nome}, seu imc é de ${resultado_imc}. Você está ${classificacao}`;
     }
     else
     {
